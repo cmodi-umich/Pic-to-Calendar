4155416e515492b9ad02886aec76749d0dd0de25
cmodi <cmodi@umich.edu>
2020-05-29 00:37:01 -0400

    having api issues

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,56 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, Button, View } from "react-native";
+import * as Google from "expo-google-app-auth";
 
-const handleSignIn = () => {
-  console.log("signing in");
-};
+async function signInWithGoogleAsync() {
+  try {
+    const result = await Google.logInAsync({
+      androidClientId:
+        "469044449243-99o0ititqfn517eh4glvrqkp37jiq5au.apps.googleusercontent.com",
+      iosClientId:
+        "469044449243-fdhtjgejhjrsfhcu80gfhiacod2ecmt9.apps.googleusercontent.com",
+      scopes: ["profile", "email"],
+    });
+
+    if (result.type === "success") {
+      return result;
+    } else {
+      return { cancelled: true };
+    }
+  } catch (e) {
+    return { error: true };
+  }
+}
 
 export default function HomeScreen({ navigation }) {
+  [accessToken, setAccessToken] = useState();
+
+  async function getSomething() {
+    return await fetch(
+      "https://www.googleapis.com/calendar/v3/users/me/calendarList",
+      {
+        headers: { Authorization: `Bearer ${accessToken.accessToken}` },
+      }
+    );
+  }
+
+  const handleSignIn = () => {
+    signInWithGoogleAsync().then((token) => {
+      setAccessToken(token);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Welcome to the GCAL App!</Text>
       <Button title='Sign In' onPress={handleSignIn} />
+      <Button title='print' onPress={() => console.log(accessToken)} />
+      <Button
+        title='get list'
+        onPress={async () => {
+          getSomething().then((res) => console.log(res.json()));
+        }}
+      />
     </View>
   );
 }
